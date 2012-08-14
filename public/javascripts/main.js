24da0b39842f89d1f3a421a0e1e8de44f35482ef
Nathan Hopkins <natehop@gmail.com>
2012-08-14 17:36:37 -0600

    Added a script to init images.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,27 @@
 (function() {
 
   function init() {
+    initTables();
+    initImages();
+  }
+
+  function initTables() {
     $(".table-awesome").dataTable({
       "bPaginate": false,
       "bInfo": true
+    });
+  }
+
+  function initImages() {
+    $("img").each(function(i, img) {
+      var
+        $img = $(img),
+        isThumbnail = $img.hasClass("thumbnail");
+
+      isThumbnail = isThumbnail || $img.parents(".thumbnail").length > 0;
+      if (!isThumbnail) {
+        $img.addClass("thumbnail");
+      }
     });
   }
 
