d1897b8fbe723ba947827c8169afce3af4d14519
Nathan Hopkins <natehop@gmail.com>
2012-08-14 18:11:14 -0600

    Added a breadcrumb initializer.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,15 +1,19 @@
 (function() {
 
   function init() {
-    initTables();
+    initBreadcrumbs();
     initImages();
+    initTables();
   }
 
-  function initTables() {
-    $(".table-awesome").dataTable({
-      "bPaginate": false,
-      "bInfo": true
-    });
+  function initBreadcrumbs() {
+    var
+      $breadcrumb = $(".breadcrumb");
+
+    // TODO: update to use a template
+    if ($breadcrumb.length === 0) {
+      $($(".container")[0]).prepend("<ul class='breadcrumb'><li><a href='http://hopsoft.github.com/'>Home</a> <span class='divider'>/</span></li><li class='active'>" + document.title + "</li></ul>");
+    }
   }
 
   function initImages() {
@@ -24,6 +28,13 @@
           $img.addClass("thumbnail");
         }
       }
+    });
+  }
+
+  function initTables() {
+    $(".table-awesome").dataTable({
+      "bPaginate": false,
+      "bInfo": true
     });
   }
 
