f05716c4f374b8bf708eec95795b8ec47d4b8e71
Nathan Hopkins <natehop@gmail.com>
2012-08-14 18:15:40 -0600

    Updated the breadcrumb init.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,7 +12,7 @@
 
     // TODO: update to use a template
     if ($breadcrumb.length === 0) {
-      $($(".container")[0]).prepend("<ul class='breadcrumb'><li><a href='http://hopsoft.github.com/'>Home</a> <span class='divider'>/</span></li><li class='active'>" + document.title + "</li></ul>");
+      $($(".container")[1]).prepend("<ul class='breadcrumb'><li><a href='http://hopsoft.github.com/'>Home</a> <span class='divider'>/</span></li><li class='active'>" + document.title + "</li></ul>");
     }
   }
 
